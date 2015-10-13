57135902ef4a47d2014b5e5f20cfafc8567c3ac7
Anthony Simmon <simmon.anthony@gmail.com>
2015-10-12 23:38:45 -0400

    Initial commit

---
+++ src/Askaiser.Android.MuPDF/PartialClasses.cs
@@ -0,0 +1,13 @@
+﻿using Android.Runtime;
+
+namespace Com.Artifex.Mupdfdemo
+{
+    partial class ReaderView
+    {
+        protected override Java.Lang.Object RawAdapter
+        {
+            get { return Adapter.JavaCast<Java.Lang.Object>(); }
+            set { Adapter = value.JavaCast<global::Android.Widget.IListAdapter>(); }
+        }
+    }
+}
---
+++ src/Askaiser.Android.MuPDF/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MuPDF for Xamarin Android")]
+[assembly: AssemblyDescription("A port of the MuPDF Android library for Xamarin Android")]
+[assembly: AssemblyCompany("Anthony Simmon")]
+[assembly: AssemblyProduct("Askaiser.Android.MuPDF")]
+[assembly: AssemblyCopyright("Copyright © 2015 by Anthony Simmon")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
