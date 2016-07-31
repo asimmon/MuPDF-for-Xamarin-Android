ac85650ebc1f1bbcd9b5c96c97a191c189955a34
Anthony Simmon <simmon.anthony@gmail.com>
2016-07-31 12:25:24 -0400

    Updated muPDF version to 1.9a, resolve compilation errors due to Xamarin Android update

--- src/Askaiser.Android.MuPDF/PartialClasses.cs
+++ src/Askaiser.Android.MuPDF/PartialClasses.cs
@@ -4,10 +4,16 @@ namespace Com.Artifex.Mupdfdemo
 {
     partial class ReaderView
     {
+        // We use GetAdapter(...) / SetAdapter created in Metadata.xml
+        // instead of the missing Adapter { get; set; }
         protected override Java.Lang.Object RawAdapter
         {
-            get { return Adapter.JavaCast<Java.Lang.Object>(); }
-            set { Adapter = value.JavaCast<global::Android.Widget.IListAdapter>(); }
+            get { return GetAdapter().JavaCast<Java.Lang.Object>(); }
+            set
+            {
+                var adapter = value.JavaCast<global::Android.Widget.IAdapter>();
+                this.SetAdapter(adapter);
+            }
         }
     }
 }
